f7f7817ac0ffd7a1998dd8723b7c64994f7a8f10
Victor <victor5055@outlook.com>
2023-01-31 11:57:30 -0500

    Adding 1st project to portfolio.

---
+++ react-portfolio-/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,18 @@
+
+//Import
+import IMG1 from '../../assets/portfolio1.jpg'
+
+
+// MY PROJECTS.....
+const data = [
+{
+    id: 1,
+image: IMG1,
+title: 'eMarket',
+github: 'https://github.com/victor5055/eMarket.git',
+demo: 'https://safe-harbor-99168.herokuapp.com/'
+},
+]
+
+//Export Portfolio
+export default Portfolio
